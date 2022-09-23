b651b514967c0fa814bb9a5042a7035a5656d450
safi1610 <101954454+safi1610@users.noreply.github.com>
2022-09-22 22:42:47 -0400

    CP470 Assignment 2 app

--- app/src/main/java/com/example/androidassignments/ListItemsActivity.java
+++ app/src/main/java/com/example/androidassignments/ListItemsActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ListItemActivity extends AppCompatActivity {
+public class ListItemsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_item);
+        setContentView(R.layout.activity_list_items);
     }
 }
